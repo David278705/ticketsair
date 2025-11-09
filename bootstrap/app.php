eeffa63df0fb50f4f896b731533a15cbb37ba016
FrankiNarvaez <frannki03@gmail.com>
2025-11-09 17:10:31 -0500

    Fix: Force HTTPS and trust Heroku proxies for production

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        // Trust Heroku proxies
+        $middleware->trustProxies(at: '*');
+        
         $middleware->alias([
         'role' => \App\Http\Middleware\EnsureUserHasRole::class,
         ]);
